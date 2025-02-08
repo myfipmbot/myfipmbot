da83f2ca484c78cf7b3e7b772dc11f4fc27ca4b5
kavi12345786 <tharushankavishka02@gmail.com>
2025-02-08 08:32:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "KAVI-EXE=Q8o32T6Y#MiZAjyqkHKgHaZYKKOi6iCwmBweIW1uVeuabt4VI3ow",
+SESSION_ID: process.env.SESSION_ID || "KAVI-EXE=frpi3Jza#lmfizzZTW_j9sec5JQXjdDYEVgPlvYhzmxhtXK_7tmk",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS || "false",
 MODE: process.env.MODE || "public",
