73b51f0984f857794139be8dad12ff914e3b2de9
myfipmbot <ktharushan536@gmail.com>
2025-03-11 23:45:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,12 +7,4 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "KAVI-EXE=jkBCWSpQ#7GVhY4I5ixyPUeQHcBFeWr-VQLNtKCWAbDvVUr6sNc4",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS || "false",
-MODE: process.env.MODE || "public",
-ALWAYS_ONLINE : process.env.ALWAYS_ONLINE || "false",
-AUTO_TYPING: process.env.AUTO_TYPING || "false",
-AUTO_RECORDING: process.env.AUTO_RECORDING || "false",
-AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
-AUTO_STATUS__MSG: process.env.AUTO_STATUS__MSG || "`SEEN YOUR STATUS BY 𝐊𝐀𝐕𝐈-𝐄𝐗𝐄🗿🍷`",
-ANTI_CALL: process.env.ANTI_CALL || "false",
 };
